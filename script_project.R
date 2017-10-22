fe65da69392a56e827b31315a2daaba6b2a363e3
francetr <tristanfrances@yahoo.fr>
2017-10-22 16:36:15 +0200

    modification de la visualisation de graphique

--- script_project.R
+++ script_project.R
@@ -1,4 +1,4 @@
-setwd("~/Bureau/m2/s3/stat/project")
+setwd("~/Bureau/m2/s3/stat/project/script/")
 ######################################
 # Imporation des données : paramètres mesurés selon mois et selon année
 ######################################
@@ -7,18 +7,7 @@ mois<-read.csv("bdd_mois_somlit.csv", header = T,  dec = ".")
 annee<-read.csv("bdd_annee_somlit.csv", header = T,  dec = ".")
 
 #############
-# Transformation des paramètres année et mois en facteur
-#############
-
-# annee$Annee<-as.factor(annee$Annee)
-# mois$Annee<-as.factor(mois$Annee); mois$Mois<-as.factor(mois$Mois)
-
-# annee$Annee<-as.numeric(annee$Annee)
-# mois$Annee<-as.numeric(mois$Annee); mois$Mois<-as.numeric(mois$Mois)
-
-
-#############
-# Analyse multivariée
+# Analyse multivariée ACP
 #############
 str(annee)
 summary(annee)
@@ -26,14 +15,22 @@ summary(annee)
 # exemple d'ACP pour Antioche pour paramètre pH et température
 
 # choix des paramètres(select) et de l'année/site
-data<-subset(annee, NOM_SITE=="Antioche", select = c("pH","Temperature", "Salinite", "Oxygene"))
+data0<-subset(annee, NOM_SITE=="Antioche" | NOM_SITE=="Eyrac", select = c("pH","Temperature", "Salinite", "Oxygene"))
+data<-subset(annee, NOM_SITE=="Antioche" | NOM_SITE=="Eyrac", select = c("Annee","NOM_SITE","pH","Temperature", "Salinite", "Oxygene"))
+# nb_na<-sum(is.na(annee)) # Calculer nombre de NA présent dans le sous ensemble
+# c("Il y a", nb_na,"NA")
 
-#nb_na<-sum(is.na(annee))
-#c("Il y a", nb_na,"NA")
-# calcul moyenne
+#############
+# Transformation des paramètres année et mois en facteur (pour une représentation utilisant s.class)
+#############
+
+#data$Annee<-as.factor(data$Annee)
+# mois$Annee<-as.factor(mois$Annee); mois$Mois<-as.factor(mois$Mois)
+
+
 
 library(FactoMineR)
-ACP<-PCA(scale(data), scale.unit = TRUE )
+ACP<-PCA(scale(data0), scale.unit = TRUE )
 round(ACP$eig,2)
 barplot(ACP$eig[,1], main="Ebouli des valeurs propres", xlab = "Composantes", ylab="Valeurs propres")
 100/ncol(data) #contrib min
@@ -45,16 +42,26 @@ coordS<-round(ACP$ind$coord[,1:2],2);coordS #Coordonnées des stations
 par(mfrow=c(1,2))
 plot(ACP,choix="var",axes= c(1,2))
 plot(ACP,choix="ind",axes= c(1,2))
-library(ade4)
-s.class(ACP$ind$coord[,c(1,2)] , fac=annee$NOM_SITE, col=c(1:20) )
-s.class(ACP$ind$coord[,c(1,2)] , fac=annee$Annee, col=c(1:20) )
 
+#library(ade4)
+#s.class(ACP$ind$coord[,c(1,2)] , fac=data$NOM_SITE, col=c(1:20) )
+#s.class(ACP$ind$coord[,c(1,2)] , fac=data$Annee, col=c(1:20) )
+
+
+###############
+# Représentations graphiques avec plot ou xyplot
+###############
+
+par(mfrow=c(1,1))
+?plot
+plot(Temperature~Annee, data=data, type = "n", main = "Température selon Année")
+with(subset(data, NOM_SITE=="Eyrac"), c(lines(Annee, Temperature, col = 1), points(Annee, Temperature, col = 1, pch = 16)))
+with(subset(data, NOM_SITE=="Antioche"), c(lines(Annee, Temperature, col = 2), points(Annee, Temperature, col = 2, pch = 16)))
+legend("topleft", legend=c("Eyrac", "Antioche"), col=c(1:2), pch = 16, lty = 1, title = "Sites")
 
 library(lattice)
-xyplot(Temperature~Annee, groups = NOM_SITE, data=annee, main="Température~Année", xlab="Années", ylab="Temps", col=c(1:4),pch=c(16:20))
+# /!\ Problème de légende
+xyplot(Temperature~Annee, groups = NOM_SITE, data=data, type = "b", main="Température~Année", xlab="Années", ylab="Températures", col=c(1:2), pch=c(16:17))
+legend("topleft", legend=c("Eyrac", "Antioche"), col=c(1:2), pch = c(16:17), title = "Sites")
 
-plot(Temperature~Annee, annee, type = "l", main = "Température selon Année")
-with(subset(annee, NOM_SITE=="Eyrac"), plot(Annee, Temperature, type = "l", points(Temperature~Annee, col = "green")))
-with(subset(annee, NOM_SITE=="Antioche"), plot(Annee, Temperature, type= "l", points(Temperature~Annee, col = "blue")))
 
-?plot
